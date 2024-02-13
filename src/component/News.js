b1364e140cd634a0e53c768ef2dfeafe326d0d33
Mukesh <myadav986@rku.ac.in>
2024-02-13 21:34:04 +0530

    i am adding .env file and secure password and apikey

--- src/component/News.js
+++ src/component/News.js
@@ -34,14 +34,18 @@ export class News extends Component {
         document.title=`${this.capitalizeFirstLetter(this.props.category)} - NewsTak`;
     }
     async updateNews(){
-      let url =`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=04373e8618eb476f830cb32eaf4ef143&page=1&pageSize=${this.props.pageSize}`;
+      this.props.setProgress(10);
+      let url =`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=1&pageSize=${this.props.pageSize}`;
       this.setState({loading:true});
       let data = await fetch(url);
+      this.props.setProgress(30);
       let parsedData = await data.json()
+      this.props.setProgress(70);
       this.setState({articles:parsedData.articles,
         totalResults:parsedData.totalResults,
         loading:false
       });
+      this.props.setProgress(100);
     }
     async componentDidMount(){
        this.updateNews();
@@ -60,7 +64,7 @@ export class News extends Component {
     
   fetchMoreData = async () => {
     this.setState({page:this.state.page + 1})
-    let url =`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=04373e8618eb476f830cb32eaf4ef143&page=1&pageSize=${this.props.pageSize}`;
+    let url =`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=1&pageSize=${this.props.pageSize}`;
       let data = await fetch(url);
       let parsedData = await data.json()
       this.setState({
